e7c4c512fa94ec83994820bdc940e5dfed20dae2
Olivia Sommer <osommer@uncc.edu>
2016-10-13 02:56:36 +0000

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # You can have the root of your site routed with "root"
+  root 'application#hello'
 end
